f40507188bdb6da345f7d43ad5bed086d09c20be
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-09-23 15:36:43 +0200

    Pridal som addlistener na showForm a xButton

--- js/script.js
+++ js/script.js
@@ -5,7 +5,26 @@ const searchForm = document.querySelector(".search-form");
 const bxSearchAlt = document.querySelector(".bx-search-alt");
 const textButton = document.querySelector(".text-button");
 const headerInput = document.querySelector(".header-input");
- 
+const showForm    = document.querySelector(".show-form");
+const formBlock   = document.querySelector(".form-block");
+const xButton   = document.querySelector(".x-button");
+
+
+xButton.addEventListener("click", function(e) {
+    formBlock.style.display = "none"
+})
+
+//show form 
+
+showForm.addEventListener("click", function(e) {
+    if( formBlock.style.display === "block" ) {
+        formBlock.style.display = "none"
+    } else {
+        formBlock.style.display = "block"
+    }
+
+});
+  
 // show input-header
 
 textButton.addEventListener("click", function(e) {
