04f682d11605682f5f6c5035dac667cf4a94d512
Stanislav Jendrišák <st.jendrisak@gmail.com>
2022-09-23 10:32:26 +0200

    Pridal som addeventlistener na .text-button ktorý zobrazuje a zakríva .header-input

--- js/script.js
+++ js/script.js
@@ -3,8 +3,23 @@
 const hideInput = document.querySelector(".hide-input");
 const searchForm = document.querySelector(".search-form");
 const bxSearchAlt = document.querySelector(".bx-search-alt");
+const textButton = document.querySelector(".text-button");
+const headerInput = document.querySelector(".header-input");
+ 
+// show input-header
+
+textButton.addEventListener("click", function(e) {
+    if( headerInput.style.display === "block" ) {
+        headerInput.style.display = "none"
+    } else {
+        headerInput.style.display = "block"
+    }
+
+});
 
 
+
+// mobile search
 
 bxSearchAlt.addEventListener("click", function(e) {
     searchForm.style.display = "flex";
